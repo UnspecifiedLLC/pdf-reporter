7f6885efe9fe0b5334e462a758b32c36d11cb508
Shannon Code <genecyber@gmail.com>
2019-09-17 12:05:11 -0400

    spawn report building operation into separate proc

---
+++ build.js
@@ -0,0 +1,39 @@
+var fileName = process.argv[2] || 'out.pdf'
+var docDefinition = require('./_tmp_report_spec.json')
+var options = {}
+
+var fonts = {
+    Roboto: {
+     normal: __dirname + '/fonts/Roboto-Regular.ttf',
+     bold: __dirname + '/fonts/Roboto-Medium.ttf',
+     italics: __dirname + '/fonts/Roboto-Italic.ttf',
+     bolditalics: __dirname + '/fonts/Roboto-MediumItalic.ttf'
+   } 
+ };
+var fs = require('fs')
+var PdfPrinter = require('pdfmake')
+var printer = new PdfPrinter(fonts)
+var pdfDoc = printer.createPdfKitDocument(docDefinition, options)
+    
+getDoc(pdfDoc, (err, buffer, pages)=>{
+    fs.writeFile(fileName || 'out.pdf', buffer, ()=>{
+        fs.unlink(__dirname + '/_tmp_report_spec.json', ()=>{})
+    })
+})
+
+function getDoc(pdfDoc, cb) {
+    // buffer the output
+    var chunks = [];
+    
+    pdfDoc.on('data', function(chunk) {
+        chunks.push(chunk);
+    });
+    pdfDoc.on('end', function() {
+        var result = Buffer.concat(chunks);
+        cb(null, result, pdfDoc._pdfMakePages);
+    });
+    pdfDoc.on('error', cb);
+    
+    // close the stream
+    pdfDoc.end();
+    }
--- index.js
+++ index.js
@@ -1,123 +1,115 @@
 // Define font files
-console.log( )
 var fonts = {
-   Roboto: {
-    normal: __dirname + '/fonts/Roboto-Regular.ttf',
-    bold: __dirname + '/fonts/Roboto-Medium.ttf',
-    italics: __dirname + '/fonts/Roboto-Italic.ttf',
-    bolditalics: __dirname + '/fonts/Roboto-MediumItalic.ttf'
-  } 
-};
-
-var PdfPrinter = require('pdfmake');
-const merge = require('easy-pdf-merge');
-var printer = new PdfPrinter(fonts);
+  Roboto: {
+   normal: __dirname + '/fonts/Roboto-Regular.ttf',
+   bold: __dirname + '/fonts/Roboto-Medium.ttf',
+   italics: __dirname + '/fonts/Roboto-Italic.ttf',
+   bolditalics: __dirname + '/fonts/Roboto-MediumItalic.ttf'
+ } 
+}
+const merge = require('easy-pdf-merge')
 var fs = require('fs');
 var docDefinition = require('./template')
 
 var options = {};
 
 function mergeDocuments(source_files, dest_file_path) {
-  merge(source_files, dest_file_path,function(err){
-    if(err) {
-      return console.log(err)
-    }
-    console.log('Success')
-  });
+ merge(source_files, dest_file_path,function(err){
+   if(err) {
+     return console.log(err)
+   }
+   console.log('Success')
+ });
 }
 
 function initEmptyPdf(header, meta, suppliedDocumentDefinition) {
-  docDefinition = suppliedDocumentDefinition || docDefinition
-  if (meta) {
-    docDefinition.info = meta
-  }
-  if (!suppliedDocumentDefinition) {
-    docDefinition.content = []
-  }
-  if (header) {
-    addHeader(header)
-  }
+ docDefinition = suppliedDocumentDefinition || docDefinition
+ if (meta) {
+   docDefinition.info = meta
+ }
+ if (!suppliedDocumentDefinition) {
+   docDefinition.content = []
+ }
+ if (header) {
+   addHeader(header)
+ }
+ saveTmpDocDef(docDefinition)
+}
+
+function saveTmpDocDef(def) {
+ fs.writeFileSync(__dirname + '/_tmp_report_spec.json', JSON.stringify(def || docDefinition, null, 4))
 }
 
 function addTitle(text, suppliedDocumentDefinition) {
-  docDefinition = suppliedDocumentDefinition || docDefinition
-  docDefinition.content.push({ text: text, style: 'header'})
+ docDefinition = suppliedDocumentDefinition || docDefinition
+ docDefinition.content.push({ text: text, style: 'header'})
+ saveTmpDocDef(docDefinition)
 }
 
 function addHeader(header, suppliedDocumentDefinition) {
-  docDefinition = suppliedDocumentDefinition || docDefinition
-  docDefinition.header = { text: header, style: 'header'}
+ docDefinition = suppliedDocumentDefinition || docDefinition
+ docDefinition.header = { text: header, style: 'header'}
+ saveTmpDocDef(docDefinition)
 }
 
 function addFooter(header, suppliedDocumentDefinition) {
-  docDefinition = suppliedDocumentDefinition || docDefinition
-  docDefinition.header = { text: header, style: 'header'}
+ docDefinition = suppliedDocumentDefinition || docDefinition
+ docDefinition.header = { text: header, style: 'header'}
+ saveTmpDocDef(docDefinition)
 }
 
 function addText(text, suppliedDocumentDefinition) {
-  docDefinition = suppliedDocumentDefinition || docDefinition
-  docDefinition.content.push(text)
+ docDefinition = suppliedDocumentDefinition || docDefinition
+ docDefinition.content.push(text)
+ saveTmpDocDef(docDefinition)
 }
 
 function addRow(date, description, image, tableDefinition) {
-  if (docDefinition.content.filter(def=>{return def.table}).length < 1) {
-    addTable(tableDefinition)
-  }
-  var row = [
-      date,
-      description,
-      image
-  ]
-  docDefinition.content.filter(def=>{return def.table})[0].table.body.push(row)
+ if (docDefinition.content.filter(def=>{return def.table}).length < 1) {
+   addTable(tableDefinition)
+ }
+ var row = [
+     date,
+     description,
+     image
+ ]
+ docDefinition.content.filter(def=>{return def.table})[0].table.body.push(row)
+ saveTmpDocDef(docDefinition)
 }
 
 function addTable(tableDefinition) {
-  docDefinition.content.push(tableDefinition || {
-    style: 'tableExample',
-    table: {
-      "dontBreakRows": true,
-      "widths": [150, 150, "*"],
-      "body": []
-    }
-  })
+ docDefinition.content.push(tableDefinition || {
+   style: 'tableExample',
+   table: {
+     "dontBreakRows": true,
+     "widths": [150, 150, "*"],
+     "body": []
+   }
+ })
+ saveTmpDocDef(docDefinition)
 }
 
-function createPdf(fileName){
-    docDefinition = JSON.parse(JSON.stringify(docDefinition))
-    var pdfDoc = printer.createPdfKitDocument(docDefinition, options);
-
-    getDoc(pdfDoc, (err, buffer, pages)=>{
-      fs.writeFileSync(fileName || 'out.pdf', buffer)
-    })
-}
-
-function getDoc(pdfDoc, cb) {
-  // buffer the output
-  var chunks = [];
-  
-  pdfDoc.on('data', function(chunk) {
-  	chunks.push(chunk);
-  });
-  pdfDoc.on('end', function() {
-  	var result = Buffer.concat(chunks);
-  	cb(null, result, pdfDoc._pdfMakePages);
-  });
-  pdfDoc.on('error', cb);
-  
-  // close the stream
-  pdfDoc.end();
+function createPdf(fileName, def) {
+ const execFile = require('child_process').execFile;
+ const child = execFile('node', [__dirname +'/build.js', fileName], (error, stdout, stderr) => {
+     if (error) {
+         console.error('stderr', stderr);
+         throw error;
+     }
+     console.log('stdout', stdout);
+ });
 }
 
 module.exports = {
-  initEmptyPdf,
-  addTable,
-  addRow,
-  createPdf,
-  mergeDocuments,
-  addHeader,
-  addText,
-  addTitle,
-  addFooter
+ initEmptyPdf,
+ addTable,
+ addRow,
+ createPdf,
+ mergeDocuments,
+ addHeader,
+ addText,
+ addTitle,
+ addFooter
 }
 
 

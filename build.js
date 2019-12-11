4a10e2d1446aae2d279fa288fad2e750d33a11e7
Shannon Code <genecyber@gmail.com>
2019-12-11 14:12:32 -0500

    Downloads image urls

--- build.js
+++ build.js
@@ -1,41 +1,47 @@
-var fileName = process.argv[2] || 'out.pdf';
-var docDefinition = require('./_tmp_report_spec.json');
-var options = {};
+var fileName = process.argv[2] || 'out.pdf'
+var eraseTmp = process.argv[3] || true
+var docDefinition = require('./_tmp_report_spec.json')
+var options = {}
 
 var fonts = {
-  Roboto: {
-    normal: __dirname + '/fonts/Roboto-Regular.ttf',
-    bold: __dirname + '/fonts/Roboto-Medium.ttf',
-    italics: __dirname + '/fonts/Roboto-Italic.ttf',
-    bolditalics: __dirname + '/fonts/Roboto-MediumItalic.ttf'
-  }
-};
-var fs = require('fs');
-var PdfPrinter = require('pdfmake');
-var printer = new PdfPrinter(fonts);
+    Roboto: {
+     normal: __dirname + '/fonts/Roboto-Regular.ttf',
+     bold: __dirname + '/fonts/Roboto-Medium.ttf',
+     italics: __dirname + '/fonts/Roboto-Italic.ttf',
+     bolditalics: __dirname + '/fonts/Roboto-MediumItalic.ttf'
+   } 
+ };
+var fs = require('fs')
+var PdfPrinter = require('pdfmake')
+var printer = new PdfPrinter(fonts)
 
-var pdfDoc = printer.createPdfKitDocument(docDefinition, options);
-
-getDoc(pdfDoc, (err, buffer, pages) => {
-  fs.writeFile(fileName || 'out.pdf', buffer, () => {
-    fs.unlink(__dirname + '/_tmp_report_spec.json', () => {});
-  });
-});
+var pdfDoc = printer.createPdfKitDocument(docDefinition, options)
+if (eraseTmp === "false") {
+  eraseTmp = false
+}    
+getDoc(pdfDoc, (err, buffer, pages)=>{
+  console.log("building", eraseTmp)
+    fs.writeFile(fileName || 'out.pdf', buffer, ()=>{
+      if (eraseTmp) {
+        fs.unlink(__dirname + '/_tmp_report_spec.json', () => { })
+      }
+    })
+})
 
 function getDoc(pdfDoc, cb) {
-  // buffer the output
-  var chunks = [];
-
-  pdfDoc.on('data', function(chunk) {
-    chunks.push(chunk);
-  });
-  pdfDoc.on('end', function() {
-    var result = Buffer.concat(chunks);
-    console.log('ENDED');
-    cb(null, result, pdfDoc._pdfMakePages);
-  });
-  pdfDoc.on('error', cb);
-
-  // close the stream
-  pdfDoc.end();
+    // buffer the output
+    var chunks = [];
+    
+    pdfDoc.on('data', function(chunk) {
+        chunks.push(chunk);
+    });
+    pdfDoc.on('end', function() {
+        var result = Buffer.concat(chunks);
+        console.log("ENDED")
+        cb(null, result, pdfDoc._pdfMakePages);
+    });
+    pdfDoc.on('error', cb);
+    
+    // close the stream
+    pdfDoc.end();
 }
--- index.js
+++ index.js
@@ -1,196 +1,219 @@
 // Define font files
+
 var fonts = {
   Roboto: {
-    normal: __dirname + '/fonts/Roboto-Regular.ttf',
-    bold: __dirname + '/fonts/Roboto-Medium.ttf',
-    italics: __dirname + '/fonts/Roboto-Italic.ttf',
-    bolditalics: __dirname + '/fonts/Roboto-MediumItalic.ttf'
-  }
-};
-const merge = require('easy-pdf-merge');
+   normal: __dirname + '/fonts/Roboto-Regular.ttf',
+   bold: __dirname + '/fonts/Roboto-Medium.ttf',
+   italics: __dirname + '/fonts/Roboto-Italic.ttf',
+   bolditalics: __dirname + '/fonts/Roboto-MediumItalic.ttf'
+ } 
+}
+const download = require('image-downloader')
+const merge = require('easy-pdf-merge')
 var fs = require('fs');
-const request = require('request');
-var docDefinition = require('./template');
-var initialized = false;
+const request = require('request').defaults({ encoding: null });
+var docDefinition = require('./template')
+var initialized = false
 var options = {};
 
 function mergeDocuments(source_files, dest_file_path) {
-  merge(source_files, dest_file_path, function(err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('Success');
-  });
+ merge(source_files, dest_file_path,function(err){
+   if(err) {
+     return console.log(err)
+   }
+   console.log('Success')
+ });
 }
 
 function cleanup() {
   fs.readdir(__dirname, (err, files) => {
-    files.forEach(file => {
-      if (file.includes('.png')) {
-        fs.unlink(__dirname + '/' + file, (err, info) => {});
-      }
-    });
+      files.forEach(file => {
+          if (file.includes('.png')) {
+            fs.unlink(__dirname + '/' + file, (err, info) => {
+              
+            })
+          }
+      });
   });
 }
 
 function initEmptyPdf(header, meta, suppliedDocumentDefinition) {
-  cleanup();
-  docDefinition = suppliedDocumentDefinition || docDefinition;
+  cleanup()
+  docDefinition = suppliedDocumentDefinition || docDefinition
   if (meta) {
-    docDefinition.info = meta;
+    docDefinition.info = meta
   }
   if (!suppliedDocumentDefinition) {
-    docDefinition.content = [];
+    docDefinition.content = []
   }
   if (header) {
-    addHeader(header);
+    addHeader(header)
   }
-  saveTmpDocDef(docDefinition);
+  saveTmpDocDef(docDefinition)
 }
 
 function saveTmpDocDef(def) {
-  fs.writeFileSync(
-    __dirname + '/_tmp_report_spec.json',
-    JSON.stringify(def || docDefinition, null, 4)
-  );
-  initialized = true;
+ fs.writeFileSync(__dirname + '/_tmp_report_spec.json', JSON.stringify(def || docDefinition, null, 4))
+ initialized = true
 }
 
 function addTitle(text, suppliedDocumentDefinition) {
-  docDefinition = suppliedDocumentDefinition || docDefinition;
-  docDefinition.content.push({ text: text, style: 'header' });
-  saveTmpDocDef(docDefinition);
+ docDefinition = suppliedDocumentDefinition || docDefinition
+ docDefinition.content.push({ text: text, style: 'header'})
+ saveTmpDocDef(docDefinition)
 }
 
 function addHeader(header, suppliedDocumentDefinition) {
-  docDefinition = suppliedDocumentDefinition || docDefinition;
-  docDefinition.header = { text: header, style: 'header' };
-  saveTmpDocDef(docDefinition);
+ docDefinition = suppliedDocumentDefinition || docDefinition
+ docDefinition.header = { text: header, style: 'header'}
+ saveTmpDocDef(docDefinition)
 }
 
 function addFooter(header, suppliedDocumentDefinition) {
-  docDefinition = suppliedDocumentDefinition || docDefinition;
-  docDefinition.header = { text: header, style: 'header' };
-  saveTmpDocDef(docDefinition);
+ docDefinition = suppliedDocumentDefinition || docDefinition
+ docDefinition.header = { text: header, style: 'header'}
+ saveTmpDocDef(docDefinition)
 }
 
 function addText(text, suppliedDocumentDefinition) {
-  docDefinition = suppliedDocumentDefinition || docDefinition;
-  docDefinition.content.push(text);
-  saveTmpDocDef(docDefinition);
+ docDefinition = suppliedDocumentDefinition || docDefinition
+ docDefinition.content.push(text)
+ saveTmpDocDef(docDefinition)
 }
 
-function download(url, dest, cb) {
-  const file = fs.createWriteStream(dest);
-  const sendReq = request.get(url);
-
-  // verify response code
-  sendReq.on('response', response => {
-    if (response.statusCode !== 200) {
-      return cb('Response status was ' + response.statusCode);
-    }
-    sendReq.pipe(file);
-  });
-
-  // close() is async, call cb after close completes
-  file.on('finish', () => file.close(cb));
-
-  // check for request errors
-  sendReq.on('error', err => {
-    fs.unlink(dest);
-    return cb(err.message);
-  });
-
-  file.on('error', err => {
-    // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    return cb(err.message);
-  });
+async function downloadIt(url, dest, cb){
+  let file = fs.createWriteStream(dest);
+  /* Using Promises so that we can use the ASYNC AWAIT syntax */
+  
+  return await new Promise((resolve, reject) => {
+      let data
+      let stream = request({
+          /* Here you should specify the exact link to the file you are trying to download */
+          uri: url,
+          headers: {
+              'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+              'Accept-Encoding': 'gzip, deflate, br',
+              'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+              'Cache-Control': 'max-age=0',
+              'Connection': 'keep-alive',
+              'Upgrade-Insecure-Requests': '1',
+              'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+          },
+          /* GZIP true for most of the websites now, disable it if you don't need it */
+          gzip: false
+      }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          data = "data:" + response.headers["content-type"] + ";base64," + new Buffer.from(body).toString('base64');
+          // console.log(data);
+        }
+      })
+      .pipe(file)
+      .on('finish', () => {
+          console.log(`The file is finished downloading.`);
+          fs.unlink(dest, ()=>{
+            resolve(data);
+          })
+          
+      })
+      .on('error', (error) => {
+          reject(error);
+      })
+  })
+  .catch(error => {
+      // console.log(`Something happened: ${error}`);
+  })
 }
 
-function addRow(date, description, image, tableDefinition) {
-  if (
-    docDefinition.content.filter(def => {
-      return def.table;
-    }).length < 1
-  ) {
-    addTable(tableDefinition);
+async function addRow(date, description, image, tableDefinition) {
+  var content = docDefinition.content[docDefinition.content.length -1]
+  if (typeof(content)!== "object" || !content || !content.table) {
+    addTable(tableDefinition)
   }
   if (image.image.includes('http')) {
-    var toDownload = image.image;
-    var fileName = image.image.split('/').reverse()[0];
-    image.image = __dirname + '/' + fileName;
-    download(toDownload, image.image, () => {
-      var row = [date, description, image];
-      docDefinition.content
-        .filter(def => {
-          return def.table;
-        })[0]
-        .table.body.push(row);
-      saveTmpDocDef(docDefinition);
-    });
-  } else {
-    var row = [date, description, image];
-    docDefinition.content
-      .filter(def => {
-        return def.table;
-      })[0]
-      .table.body.push(row);
-    saveTmpDocDef(docDefinition);
-  }
+    var toDownload = image.image
+    var fileName = image.image.split('/').reverse()[0]
+    image.image = __dirname + '/' + fileName
+    await downloadIt(toDownload, image.image).then((data)=>{
+      image.image = data
+      var row = [
+        date,
+        description,
+        image
+      ]
+      content = docDefinition.content.filter(def=>{return def.table})
+      content[content.length -1].table.body.push(row)
+      saveTmpDocDef(docDefinition)
+    })
+    
+    
+ } else {
+      var row = [
+        date,
+        description,
+        image
+      ]
+      content = docDefinition.content.filter(def=>{return def.table})
+      content[content.length -1].table.body.push(row)
+      saveTmpDocDef(docDefinition)
+    }
 }
 
 function addTable(tableDefinition) {
-  docDefinition.content.push(
-    tableDefinition || {
-      style: 'tableExample',
-      table: {
-        dontBreakRows: true,
-        widths: [150, 150, '*'],
-        body: []
-      }
-    }
-  );
-  saveTmpDocDef(docDefinition);
+ docDefinition.content.push(tableDefinition || {
+   style: 'tableExample',
+   table: {
+     "dontBreakRows": true,
+     "widths": [150, 150, "*"],
+     "body": []
+   }
+ })
+ saveTmpDocDef(docDefinition)
 }
 
-function createPdf(fileName, cb) {
+function createPdf(fileName, eraseTmp, cb) {
+  console.log("erase?", eraseTmp)
+  if (eraseTmp === undefined) {
+    eraseTmp = true;
+  }
   const execFile = require('child_process').execFile;
   if (initialized) {
-    doExec(fileName);
+      doExec(fileName, eraseTmp)
   } else {
-    initEmptyPdf('Empty');
-    doExec(fileName);
+    initEmptyPdf("Empty")
+    doExec(fileName, eraseTmp)
   }
-
-  function doExec(fileName) {
+ 
+  function doExec(fileName, eraseTmp) {
     execFile(
       'node',
-      [__dirname + '/build.js', fileName],
+      [__dirname + '/build.js', fileName, eraseTmp],
       (error, stdout, stderr) => {
         if (error) {
           console.error('stderr', stderr);
           throw error;
         }
         console.log('stdout', stdout.trim());
-        if (stdout.trim() === 'Ended') {
+        if (stdout.trim() === 'ENDED') {
           console.log('Detected Ended');
-          initialized = false;
+          initialized = false
           return cb(fileName);
         }
+        console.log(stdout)
       }
     );
   }
 }
 
 module.exports = {
-  initEmptyPdf,
-  addTable,
-  addRow,
-  createPdf,
-  mergeDocuments,
-  addHeader,
-  addText,
-  addTitle,
-  addFooter
-};
+ initEmptyPdf,
+ addTable,
+ addRow,
+ createPdf,
+ mergeDocuments,
+ addHeader,
+ addText,
+ addTitle,
+ addFooter
+}
+
+
